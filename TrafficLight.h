ae6188b7d00757284d8938123847e0b3932b0506
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-02-24 10:51:18 -0600

    Update TrafficLight.h

--- TrafficLight.h
+++ TrafficLight.h
@@ -1,10 +1,24 @@
 #ifndef TRAFFICLIGHT_H_
 #define TRAFFICLIGHT_H_
 
+#include "Intersection.h"
 
 class TrafficLight
 {
+	private:
+		Intersection *intersectInstance;
+		enum LightState
+		{
+			green,
+			red,
+			yellow
+		};
 
+	public:
+		void upDate();
+		LightState getNSState();
+		LightState getEWState();
 };
+
 
 #endif
