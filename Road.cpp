481dbf56fc4ebdbbbdb76754f6ee22890e13a89b
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-02-24 23:39:43 -0600

    Update Road.cpp

--- Road.cpp
+++ Road.cpp
@@ -2,3 +2,107 @@
 
 using namespace std;
 
+Road::Road()
+{
+
+}
+
+Road::~Road()
+{
+
+}
+
+bool Road::isPointOnRoad()
+{
+
+}
+
+void Road::setRoadName(string name)
+{
+	roadName = name;
+}
+
+string Road::getRoadName()
+{
+	return roadName;
+}
+
+void Road::setxRoadStart(double startingPoint)
+{
+	xStartPoint = startingPoint;
+}
+
+double Road::getxRoadStart()
+{
+	return xStartPoint;
+}
+
+void Road::setyRoadStart(double startingPoint)
+{
+	yStartPoint = startingPoint;
+}
+
+double Road::getyRoadStart()
+{
+	return yStartPoint;
+}
+
+void Road::setxRoadEnd(double endingPoint)
+{
+	xEndPoint = endingPoint;
+}
+
+double Road::getxRoadEnd()
+{
+	return xEndPoint;
+}
+
+void Road::setyRoadEnd(double endingPoint)
+{
+	yEndPoint = endingPoint;
+}
+
+double Road::getyRoadEnd()
+{
+	return yEndPoint;
+}
+
+void Road::setLaneNumber(int lane)
+{
+	laneNumber = lane;
+}
+
+int Road::getLaneNumber()
+{
+	return laneNumber;
+}
+
+void Road::setStartIntersection(int startingPoint)
+{
+	startIntersection = startingPoint;
+}
+
+int Road::getStartIntersection()
+{
+	return startIntersection;
+}
+
+void Road::setEndIntersection(int endingPoint)
+{
+	endIntersection = endingPoint;
+}
+
+int Road::getEndIntersection()
+{
+	return endIntersection;
+}
+
+void Road::setSpeedLimit(int limit)
+{
+	speedLimit = limit;
+}
+
+int Road::getSpeedLimit()
+{
+	return speedLimit;
+}
