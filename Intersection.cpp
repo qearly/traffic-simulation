9f4d7449477cb391193499d34cfeb7689516d6c9
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-02-25 20:12:56 -0600

    Update Intersection.cpp

--- Intersection.cpp
+++ Intersection.cpp
@@ -1,3 +1,63 @@
 #include "Intersection.h"
 
 using namespace std;
+
+bool Intersection::isPointInIntersection()
+{
+	return false;
+}
+
+void Intersection::upDate()
+{
+
+}
+
+void Intersection::setIntersectionID(int ID)
+{
+
+}
+
+void Intersection::setxCenterPoint(int point)
+{
+
+}
+
+void Intersection::setyCenterPoint(int point)
+{
+
+}
+
+void Intersection::setNameNS(string name)
+{
+
+}
+
+void Intersection::setNameEW(string name)
+{
+
+}
+
+int Intersection::getIntersectionID()
+{
+	return 0;
+}
+
+int Intersection::getxCenterPoint()
+{
+	return 0;
+}
+
+int Intersection::getyCenterPoint()
+{
+	return 0;
+}
+
+string Intersection::getNameNS()
+{
+	return string();
+}
+
+string Intersection::getNameEW()
+{
+	return string();
+}
