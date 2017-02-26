8722a5c1e14e0e74ac6f69d0ad3bb9596e252526
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-02-25 20:02:03 -0600

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,3 +1,103 @@
 #include "Vehicle.h"
 
 using namespace std;
+
+void Vehicle::Move()
+{
+
+}
+
+void Vehicle::PrintReport()
+{
+
+}
+
+void Vehicle::setType(string name)
+{
+
+}
+
+void Vehicle::setCarID(int ID)
+{
+
+}
+
+string Vehicle::getType()
+{
+	return string();
+}
+
+int Vehicle::getCarID()
+{
+	return 0;
+}
+
+void Vehicle::setxIntersectStartPoint(double start)
+{
+
+}
+
+void Vehicle::setyIntersectStartPoint(double start)
+{
+
+}
+
+void Vehicle::setIntersectStartDirection(int start)
+{
+
+}
+
+void Vehicle::setIntersectAcceleration(double acceleration)
+{
+
+}
+
+void Vehicle::setIntersectSpeed(double speed)
+{
+
+}
+
+void Vehicle::setxIntersectLocation(double location)
+{
+
+}
+
+void Vehicle::setyIntersectLocation(double location)
+{
+
+}
+
+double Vehicle::getxIntersectionStartPoint()
+{
+	return 0.0;
+}
+
+double Vehicle::getyIntersectionStartPoint()
+{
+	return 0.0;
+}
+
+int Vehicle::getIntersectStartDirection()
+{
+	return 0;
+}
+
+double Vehicle::getIntersectAcceleration()
+{
+	return 0.0;
+}
+
+double Vehicle::getIntersectSpeed()
+{
+	return 0.0;
+}
+
+double Vehicle::getxIntersectLocation()
+{
+	return 0.0;
+}
+
+double Vehicle::getyIntersectLocation()
+{
+	return 0.0;
+}
