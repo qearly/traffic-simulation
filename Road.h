c3bc2e7f7547a6851caa3ee619c70146bfeaa072
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-02-26 14:42:28 -0600

    Update Road.h

--- Road.h
+++ Road.h
@@ -7,19 +7,30 @@ class Road
 {
 	private:
 		string roadName;
+		bool directionNS;
+		bool directionEW;
 		double xStartPoint;
 		double yStartPoint;
-		double xEndpoint;
+		double xEndPoint;
 		double yEndPoint;
+		double xCoord;
+		double yCoord;
 		int laneNumber;
 		int startIntersection;
 		int endIntersection;
 		int speedLimit;
 
 	public:
+		Road();
+		~Road();
 		bool isPointOnRoad();
+		void setDirectionNS(bool name);
+		void setDirectionEW(bool name);
+		bool getDirectionNS();
+		bool getDirectionEW();
 
-		void setxRoadStart (double startingPoint);
+		void setRoadName(string name);
+		void setxRoadStart(double startingPoint);
 		void setxRoadEnd(double endingPoint);
 		void setyRoadStart(double startingPoint);
 		void setyRoadEnd(double endingPoint);
@@ -27,15 +38,20 @@ class Road
 		void setStartIntersection(int startingPoint);
 		void setEndIntersection(int endingPoint);
 		void setSpeedLimit(int limit);
+		void setxCoord(double x);
+		void setyCoord(double y);
 
+		string getRoadName();
 		double getxRoadStart();
 		double getxRoadEnd();		
-		double getyRoadEnd();
+		double getyRoadStart();
 		double getyRoadEnd();
 		int getLaneNumber();		
 		int getStartIntersection();		
 		int getEndIntersection();		
 		int getSpeedLimit();
+		double getxCoord();
+		double getyCoord();
 };
 
 
