1ec568d661fa6a2b640b075dee90fa37085dc8ed
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-02-25 19:45:15 -0600

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -1,20 +1,46 @@
 #ifndef VEHICLE_H_
 #define VEHICLE_H_
 
+#include <string>
+
 class Vehicle
 {
-  private:
+	private:
 		string type;
 		int carID;
-		int startPoint;
-		int startDirection;
-		double acceleration;
-		int speed;
-		int location;
+		double xIntersectStartPoint;
+		double yIntersectStartPoint;
+		int intersectStartDirection;
+		double intersectAcceleration;
+		double intersectSpeed;
+		double xintersectLocation;
+		double yintersectLocation;
 
 	public:
 		void Move();
 		void PrintReport();
+
+		void setType(string name);
+		void setCarID(int ID);
+		string getType();
+		int getCarID();
+
+		void setxIntersectStartPoint(double start);
+		void setyIntersectStartPoint(double start);
+		void setIntersectStartDirection(int start);
+		void setIntersectAcceleration(double acceleration);
+		void setIntersectSpeed(double speed);
+		void setxIntersectLocation(double location);
+		void setyIntersectLocation(double location);
+
+		double getxIntersectionStartPoint();
+		double getyIntersectionStartPoint();
+		int getIntersectStartDirection();
+		double getIntersectAcceleration();
+		double getIntersectSpeed();
+		double getxIntersectLocation();
+		double getyIntersectLocation();
+
 };
 
 
