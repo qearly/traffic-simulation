4c24d791b16d5aba8c178ed754e0146fbc32c74d
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-02-25 18:21:51 -0600

    Update Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -1,3 +1,53 @@
 #include "Map.h"
 
 using namespace std;
+
+Map::Map(TrafficSimDataParser *db)
+{
+	
+}
+
+Map::~Map()
+{
+
+}
+
+Road * Map::getRoad(char * rdID)
+{
+	
+}
+
+Road * Map::getRoad(double x, double y, double dir)
+{
+	
+}
+
+void Map::addIntersection(Intersection * i)
+{
+
+}
+
+Intersection * Map::getIntersection(int id)
+{
+	
+}
+
+Intersection * Map::getNextIntersection(double x, double y, double dir)
+{
+	
+}
+
+Intersection * Map::getIntersection(double x, double y)
+{
+	
+}
+
+void Map::PrintReport()
+{
+
+}
+
+void Map::upDate(double time)
+{
+
+}
