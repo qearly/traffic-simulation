d062bb2eeddd1ef5230def2a35b9043578edfe4d
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-02-27 10:18:12 -0600

    Update Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -4,7 +4,37 @@ using namespace std;
 
 Map::Map(TrafficSimDataParser *db)
 {
-	
+	// Collection of Roads
+	int rCount = dp->getRoadCount();
+	char roadName[64];
+	double xStart;
+	double yStart;
+	double xEnd;
+	double yEnd;
+	int intersectStart;
+	int intersectEnd;
+	double speedLimit;
+	int numLanes;
+
+	for (int i = 0; i <= rCount; i++)
+	{ 
+		dp->getRoadData(roadName, &xStart, &yStart, &xEnd, &yEnd, &intersectStart, &intersectEnd, &speedLimit, &numLanes);
+	}
+
+	// Collection of Intersections
+	int iCount = dp->getIntersectionCount();
+	int ID;
+	double xPosition;
+	double yPosition;
+	char nameN[32];
+	char nameE[32];
+	char nameS[32];
+	char nameW[32];
+
+	for (int j = 0; j <= iCount; j++)
+	{
+		dp->getIntersectionData(&ID, &xPosition, &yPosition, nameN, nameE, nameS, nameW);
+	}
 }
 
 Map::~Map()
