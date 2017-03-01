1107b4dde4b94c9729f39c95a0da68b23d705269
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-02-28 22:30:42 -0600

    Update TrafficLight.cpp

--- TrafficLight.cpp
+++ TrafficLight.cpp
@@ -1,3 +1,54 @@
 #include "TrafficLight.h"
 
 using namespace std;
+
+void TrafficLight::trafficUpDate()
+{
+
+}
+
+TrafficLight::LightState TrafficLight::getNSState()
+{
+	if (GREEN == true)
+	{
+		// Start timer for GREEN
+		// Once timer passes the alloted time for a green light, set GREEN to false
+	}
+
+	else if (RED == true)
+	{
+		// Start timer for RED
+		// Once timer passes the alloted time for a red light, set red to false
+	}
+
+	else if (YELLOW == true)
+	{
+		// Start timer for YELLOW
+		// Once timer passes the alloted time for a yellow light, set YELLOW to false
+	}
+}
+
+TrafficLight::LightState TrafficLight::getEWState()
+{
+	if (GREEN == true)
+	{
+		// Start timer for GREEN
+		// Once timer passes the alloted time for a green light, set GREEN to false
+
+		return GREEN;
+	}
+
+	else if (RED == true)
+	{
+		// Start timer for RED
+		// Once timer passes the alloted time for a red light, set red to false
+
+		return RED;
+	}
+
+	else if (YELLOW == true)
+	{
+		// Start timer for YELLOW
+		// Once timer passes the alloted time for a yellow light, set YELLOW to false
+	}
+}
