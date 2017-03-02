c277274c5e0d00f1b3e36fbacad70b05c0e78d55
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-03-02 01:55:49 -0600

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -14,7 +14,76 @@ void Vehicle::PrintReport()
 
 void Vehicle::setVehicleSpeed(double mps, double mph)
 {
+	double laneWidth = 36.6;
+	double BC = ((roadInst->getLaneNumber()) == 4) ? (laneWidth * 2.0) : (laneWidth * 1.0);
+	double AC = intersectInst->getxCenterPoint() - intersectInst->getyCenterPoint();
+	double distNextIntersection = AC - BC;
+	double speedLimitMPS = roadInst->getSpeedLimitMPS();
+	double speedLimitMPH = roadInst->getSpeedLimitMPH();
+	double stopDist = 1.1 * speedMPH + 0.06 * pow(speedMPH, 2);
 
+	// Becai\use it's returning an enum I am not yet sure how to set this to a variable
+	lightInst->getEWState();
+
+	if ((lightInst->getEWState() == RED) || (lightInst->getEWState() == YELLOW))
+	{
+		if (stopDist < distNextIntersection)
+		{
+			if (speedMPS < speedLimitMPS)
+			{
+				speedMPS += Acceleration;
+				speedMPH += speedMPS / 0.44704;
+				if (speedMPS > speedLimitMPS)
+				{
+					speedMPS = speedLimitMPS;
+					speedMPH = speedMPS / 0.44704;
+				}
+			}
+		}
+		
+		else if ((stopDist > distNextIntersection) && (speedMPS != 0))
+		{
+			while ((stopDist > distNextIntersection) && (distNextIntersection > laneWidth) && (speedMPS > 0))
+			{
+				speedMPS -= Acceleration;
+				speedMPH = speedMPS / 0.44704;
+				if (speedMPS < 0)
+				{
+					speedMPS = 0;
+					speedMPH = 0;
+				}
+				distNextIntersection = stopDist = 1.1 * speedMPH + 0.06 * pow(speedMPH, 2);
+				distNextIntersection *= 0.3048;
+				if (speedMPS == 0)
+				{
+					break;
+				}
+			}
+
+			if (distNextIntersection < laneWidth)
+			{
+				speedMPS = 0;
+				speedMPH = 0;
+			}
+		 }
+
+		else
+		{
+			if (speedMPS < speedLimitMPS)
+			{
+				speedMPS += Acceleration;
+				speedMPH = speedMPS / 0.44704;
+				if (speedMPS > speedLimitMPS)
+				{
+					speedMPS = speedLimitMPS;
+					speedMPH = speedMPS / 0.44704;
+				}
+			}
+		}
+
+		setSpeedMPS(speedMPS);
+		setSpeedMPH(speedMPH);
+	}
 }
 
 void Vehicle::setType(char * name)
