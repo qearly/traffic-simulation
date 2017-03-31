ea7c3965bd6678385ee5ce930891d56040ad8489
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-03-31 11:57:48 -0500

    Update Intersection.h

--- Intersection.h
+++ Intersection.h
@@ -7,26 +7,49 @@ class Intersection
 {
 	private:
 		int intersectionID;
-		int xCenterPoint;
-		int yCenterPoint;
-		string nameNS;
-		string nameEW;
+		double xCenterPoint;
+		double yCenterPoint;
+		double xIntersectCoord;
+		double yIntersectCoord;
+		int NumLanesNS;
+		int NumLanesEW;
+		char nameN[64];
+		char nameS[64];
+		char nameE[64];
+		char nameW[64];
+		char curRoad[64];
 
 	public:
-		bool isPointInIntersection();
-		void upDate();
-
+		Intersection();
+		~Intersection();
+		bool isPointInIntersection(double xCenterPoint, double yCenterPoint);
+		void intersectUpDate(TrafficLight *light);
 		void setIntersectionID(int ID);
-		void setxCenterPoint(int point);
-		void setyCenterPoint(int point);
-		void setNameNS(string name);
-		void setNameEW(string name);
+		void setxCenterPoint(double point);
+		void setyCenterPoint(double point);
+		void setxIntersectCoord(double x);
+		void setyIntersectCoord(double y);
+		void setNumLanesNS(int num);
+		void setNumLanesEW(int num);
+		void setNameN(char *name);
+		void setNameS(char *name);
+		void setNameE(char *name);
+		void setNameW(char *name);
+		void setCurRoad(char *road);
+		void setIntersectionData(int intersectID, double xPosition, double yPosition, char *north, char *east, char *south, char *west);
 
 		int getIntersectionID();
-		int getxCenterPoint();
-		int getyCenterPoint();
-		string getNameNS();
-		string getNameEW();
+		double getxCenterPoint();
+		double getyCenterPoint();
+		double getxIntersectCoord();
+		double getyIntersectCoord();
+		int getNumLanesNS();
+		int getNumLanesEW();
+		char *getNameN();
+		char *getNameS();
+		char *getNameE();
+		char *getNameW();
+		char *getCurRoad();
 };
 
 #endif
