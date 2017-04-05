b415eb49bf434efabd9c0260bf7531e53bbc29de
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-04-05 01:15:31 -0500

    Add files via upload

---
+++ FourCylinderMovement.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Movement.h"
+#include "TrafficLight.h"
+#include "Road.h"
+#include "TrafficSimDataParser.h"
+#include "Map.h"
+
+
+class FourCylinderMovement : public Movement
+{
+	private:
+		int counter;
+		int max;
+		double value;
+		bool turnDecided = false;
+		enum turnState { LEFT, STRAIGHT, RIGHT };
+		TrafficLight *light;
+		Road *road;
+		TrafficSimDataParser *dp;
+		Intersection *intersection;
+		Map *mapInst;
+		Intersection *nextIntersection;
+
+	protected:
+		turnState turnDirection;
+
+	public:
+		FourCylinderMovement();
+		~FourCylinderMovement();
+		void Move(char *filename);
+};
