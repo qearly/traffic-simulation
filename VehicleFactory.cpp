81d48667d79e3ae9f009610cd5f6996561ff5cfa
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-04-05 14:47:26 -0500

    Add files via upload

---
+++ VehicleFactory.cpp
@@ -0,0 +1,21 @@
+#include "VehicleFactory.h"
+
+VehicleFactory::~VehicleFactory()
+{
+
+}
+
+Vehicle * VehicleFactory::getVehicle(char * name)
+{
+	return nullptr;
+}
+
+Vehicle * VehicleFactory::getInstance()
+{
+	return nullptr;
+}
+
+Vehicle * VehicleFactory::createVehicle()
+{
+	return nullptr;
+}
