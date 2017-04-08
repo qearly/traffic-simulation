6540cfa8608d332564998f0d4c744de721967a9e
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-04-08 16:32:54 -0500

    Add files via upload

---
+++ EightCylinderMovement.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Vehicle.h"
+#include "Movement.h"
+#include "TrafficLightManager.h"
+#include "Road.h"
+#include "TrafficSimDataParser.h"
+#include "Map.h"
+
+class Vehicle;
+class EightCylinderMovement : public Movement
+{
+private:
+	int counter = 0;
+	int max = 100;
+	double value = 0;
+	bool turnDecided = false;
+	enum turnState { LEFT, STRAIGHT, RIGHT };
+	Vehicle *vehicle;
+	TrafficLightManager *light;
+	Road *road;
+	TrafficSimDataParser *dp;
+	Intersection *intersection;
+	Map *mapInst;
+	Intersection *nextIntersection;
+
+protected:
+	turnState turnDirection;
+
+public:
+	void Move(char *filename);
+	void PrintStatus();
+};
