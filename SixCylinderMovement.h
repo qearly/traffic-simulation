b73013d265bf9afc9b58167692a646a9a560c708
Matt Stewart <mattstewart32@users.noreply.github.com>
2017-07-23 18:59:35 -0500

    Create SixCylinderMovement.h

---
+++ SixCylinderMovement.h
@@ -0,0 +1,31 @@
+#include "Vehicle.h"
+#include "Movement.h"
+#include "TrafficLightManager.h"
+#include "Road.h"
+#include "TrafficSimDataParser.h"
+#include "Map.h"
+
+class Vehicle;
+class SixCylinderMovement : public Movement
+{
+private:
+	int counter;
+	int max;
+	double value;
+	bool turnDecided;
+	enum turnState { LEFT, STRAIGHT, RIGHT };
+	Vehicle *vehicle;
+	TrafficLightManager *light;
+	Road *road;
+	TrafficSimDataParser *dp;
+	Intersection *intersection;
+	Map *mapInst;
+	Intersection *nextIntersection;
+
+protected:
+	turnState turnDirection;
+
+public:
+	void Move(char *filename);
+	void PrintStatus();
+};
